abe551b25d8a4893f3d3f595f066af764e67051d
jaber1104076 <jabercuet11@gmail.com>
2023-03-16 15:39:01 +0600

    pricing added

--- src/Main/Layout.js
+++ src/Main/Layout.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import PricingInfo from '../Pages/Home/Pricing/PricingInfo';
 import Footer from '../Shared/Footer/Footer';
 import Navbar from '../Shared/Navbar/Navbar';
 const Layout = () => {
     return (
         <div>
-            <Navbar/>
-            <Outlet/>
-            <Footer/>
+            <Navbar />
+            <Outlet />
+            <PricingInfo></PricingInfo>
+            <Footer />
         </div>
     );
 };
